5768a7f56b5e6b995b01b6374f6af05d71650b91
erlend <erlend@leikn.es>
2021-03-15 15:45:32 +0100

    Updating structure for pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='burpy',
+    version='0.9',
+    packages=[''],
+    url='https://github.com/elnerd/burpy',
+    license='MIT',
+    author='Erlend Leiknes',
+    description='Burp interface with typing hints'
+)
